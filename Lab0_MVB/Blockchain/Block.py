c86a10c7ff25d095c6da043ddc20dfd69ce8f42b
allenchan <allenchan@chenpangzideiMac.local>
2020-09-03 12:18:29 -0400

    upload lab0

---
+++ Lab0_MVB/Blockchain/Block.py
@@ -0,0 +1,23 @@
+import json
+
+from Blockchain import Transaction
+
+
+class Block:
+    def __init__(self, tx: Transaction, prev, nonce, pow):
+        self.tx = tx  # <a single transaction>
+        self.prev = prev  # <hash of the previous block>
+        self.nonce = nonce  # <the nonce value, used for proof-of-work>
+        self.pow = pow  # <the proof-of-work, a hash of the tx, prev, and nonce fields>
+
+    def getJson(self):
+        jsonObj = {"tx": self.tx.getJson(), "prev": self.prev, "nonce": self.nonce, "pow": self.pow}
+        return json.dumps(jsonObj)
+
+
+class BlockTreeNode:
+    def __init__(self, prevBlock, nowBlock: Block, nextBlock, blockHeight: int):
+        self.prevBlock = prevBlock
+        self.nowBlock = nowBlock
+        self.nextBlock = nextBlock
+        self.blockHeight = blockHeight
