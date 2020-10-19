eec580f7b35e19f999d418954ae3bff59482ab56
allenchan <zchen85@jhu.edu>
2020-10-19 12:50:41 -0400

    dao updated

--- Ethereum/dao.sol
+++ Ethereum/dao.sol
@@ -14,25 +14,28 @@ contract Dao {
     The default curator is the creator */
     address private curator;
     /** the proposal carried by the contract */
-    Proposal private proposal;
+    Proposal private curProposal;
+    /** record all addresses that have tokens */
     address[] addresses;
 
     /* define the Proposal struct, which contains
     the latest status of a proposal */
     struct Proposal {
         bool isSealed;        /** whether the proposal is sealed or unsealed */
-        uint256 totalYes;
-        uint256 totalNo;
-        mapping (address => bool) voteRecord;
-        mapping (address => bool) didVote;
+        uint256 totalYes;     /** number of yes votes on the proposal */
+        uint256 totalNo;      /** number of no votes on the proposal */
+        mapping (address => bool) voteRecord; /** record each address's vote choice, 
+        true for yes vote, false for no vote */
+        mapping (address => bool) didVote;    /** record whether each address has
+        voted or not, true for has voted, false for hasn't voted */
         uint256 threshold;  /** threshold is the number of tokens which 
         existed at the time of the proposal's creation */
     }
 
     constructor() public {
-        creator = msg.sender;
-        curator = creator;
-        proposal.isSealed = true; /** initial the proposal is sealed */
+        creator = msg.sender;     /** initialize proposal creator */
+        curator = creator;        /** initially the curator is the creator */
+        curProposal.isSealed = true; /** initially the proposal is sealed */
     }
 
     /** The curator can call this function to change the
@@ -40,7 +43,7 @@ contract Dao {
     function delegateCurator(address newCurator) public {
         /** check the sender is the current curator
         and the proposal is sealed */
-        require (curator == msg.sender && proposal.isSealed);
+        require (curator == msg.sender && curProposal.isSealed);
         curator = newCurator;
     }
 
@@ -50,104 +53,113 @@ contract Dao {
         uint256 tokensGained = msg.value / valuation;
         totalBalance += tokensGained;
         balances[msg.sender] += tokensGained;
+        /** push new address */
+        if (!addressExists(msg.sender)) {
+            addresses.push(msg.sender);
+        }
+
         /** A deposit made by someone who has voted also commits 
         the new tokens to their vote */
-        if (!proposal.isSealed && proposal.didVote[msg.sender]) {
-            if (proposal.voteRecord[msg.sender]) {
-                proposal.totalYes += tokensGained;
-            } else if (!proposal.voteRecord[msg.sender]) {
-                proposal.totalNo += tokensGained;
+        if (!curProposal.isSealed && curProposal.didVote[msg.sender]) {
+            if (curProposal.voteRecord[msg.sender]) {
+                curProposal.totalYes += tokensGained;
+            } else if (!curProposal.voteRecord[msg.sender]) {
+                curProposal.totalNo += tokensGained;
             }
             /** check latest vote result */
             checkVoteResult();
         }
     }
 
-    function withdraw(uint256 tokensToWithdraw) public returns (bool _bool){
+    /** pays eth out at a rate of valuation for a specified number of tokens */
+    function withdraw(uint256 tokensToWithdraw) public returns (bool _bool) {
         require (balances[msg.sender] > tokensToWithdraw);
         require (totalBalance >= tokensToWithdraw);
         require (tokensToWithdraw > 0);
-        // require (!checkNewAccount(msg.sender));  // only the existed account can withdraw
+        require (addressExists(msg.sender)); /** only existing address can withdraw tokens */
         
         /** Cannot withdraw tokens if the proposal is unsealed 
         and the address has voted, so it's valid withdraw
         operation only when proposal is sealed or proposal
         is unsealed but the address hasn't voted yet. */
-        require (proposal.isSealed || (!proposal.didVote[msg.sender]));
+        require (curProposal.isSealed || (!curProposal.didVote[msg.sender]));
         
         balances[msg.sender] -= tokensToWithdraw;
         msg.sender.transfer(tokensToWithdraw * valuation);
         return true;
     }
 
-    function getBalance() public view returns (uint256) {
+    function getBalance() public view returns (uint256 _balance) {
         return balances[msg.sender];
     }
     
+    /** only curator can create a new proposal and the proposal must be sealed */ */
     function createProposal() public {
-        require (msg.sender == curator);
-        require (proposal.isSealed);
-        proposal.isSealed = false;
-        proposal.threshold = totalBalance / 2;
-    }
-    
-    function vote(bool voteChoice) public {
-        require (!proposal.isSealed);
-        require (balances[msg.sender] > 0);
-        require (!proposal.didVote[msg.sender]);
-        
-        /** set the vote status of the address to true */
-        proposal.didVote[msg.sender] = true;
-        if (voteChoice) {
-            proposal.voteRecord[msg.sender] = true;
-            proposal.totalYes += balances[msg.sender];
-        } else {
-            proposal.voteRecord[msg.sender] = false;
-            proposal.totalNo += balances[msg.sender];
-        }
-        checkVoteResult();
-    }
-
-    function checkVoteResult() private {
-        if (proposal.totalYes > proposal.threshold) {
-            valuation = valuation * getRandom() / 10;
-            if (valuation == 0) {
-               resetBalance();
-            }
-            resetProposal();
-        } else if (proposal.totalNo > proposal.threshold) {
-            resetProposal();
-        }
-    }
-    
-    function resetProposal() private {
-        proposal = Proposal({
-            isSealed: true,
+        require (curator == msg.sender && curProposal.isSealed);
+        delete curProposal;
+        curProposal = Proposal({
+            isSealed: false,
             totalYes: 0,
             totalNo: 0,
             threshold: totalBalance / 2
         });
         for (uint i = 0; i < addresses.length; i++) {
-            proposal.didVote[addresses[i]] = false;
-            proposal.voteRecord[addresses[i]] = false;
+            curProposal.didVote[addresses[i]] = false;
+            curProposal.voteRecord[addresses[i]] = false;
+        }
+    }
+
+    /** commits a user's tokens to yes or no for a given proposal */
+    function vote(bool voteChoice) public {
+        require (!curProposal.isSealed);
+        require (balances[msg.sender] > 0);
+        require (!curProposal.didVote[msg.sender]);
+        require (addressExists(msg.sender)); /** only existing address can vote */
+        
+        /** set the vote status of the address to true */
+        curProposal.didVote[msg.sender] = true;
+        /** mark vote record of the current address and update total vote count */
+        if (voteChoice) {
+            curProposal.voteRecord[msg.sender] = true;
+            curProposal.totalYes += balances[msg.sender];
+        } else {
+            curProposal.voteRecord[msg.sender] = false;
+            curProposal.totalNo += balances[msg.sender];
+        }
+        checkVoteResult();
+    }
+
+    /** check vote result of the current proposal */
+    function checkVoteResult() private {
+        if (curProposal.totalYes > curProposal.threshold) {
+            valuation = valuation * getRandom() / 10;
+            /** If valuation hits 0.0 all tokens are invalidated */
+            if (valuation == 0) {
+                totalBalance = 0;
+                valuation = 1;
+                for (uint i = 0; i < addresses.length; i++) {
+                    balances[addresses[i]] = 0;
+                }
+                delete addresses;
+            }
+            sealProposal();
+        } else if (curProposal.totalNo > curProposal.threshold) {
+            sealProposal();
         }
     }
     
-    function resetBalance() private {
-        totalBalance = 0;
-        for (uint i = 0; i < addresses.length; i++) {
-            balances[addresses[i]] = 0;
-        }
-        valuation = 1;
+    function sealProposal() private {
+        curProposal.isSealed = true;
     }
     
-    function checkNewAccount(address newAddress) private view returns(bool) {
+    /** check whether the address exists, i.e. it has tokens */
+    function addressExists(address addr) private view returns (bool) {
         for (uint i = 0; i < addresses.length; i++) {
-            if (addresses[i] == newAddress) {
-                return false;
+            if (addresses[i] == addr) {
+                return true;
             }
         }
-        return true;
+        return false;
     }
 
     function getRandom() private pure returns(uint) {
